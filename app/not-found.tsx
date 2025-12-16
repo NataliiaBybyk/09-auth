e14c8e493745e3a0d06ccb68fb183ff417963040
Nataliia Bybyk <natashakorobochka@gmail.com>
2025-12-16 08:42:14 +0300

    add

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,25 @@
 import css from './Home.module.css'
+import { Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'Page Not Found - NoteHub',
+  description: 'The page you are looking for does not exist',
+  openGraph:{
+    title: 'Page Not Found - NoteHub',
+  description: 'The page you are looking for does not exist',
+  url:"",
+   images:[
+  {
+    url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+    width: 1200,
+    height: 630,
+    alt: "Page Not Found - NoteHub"
+  }
+ ]
+  }
+ 
+};
 
 function NotFound(){
     return (
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -6,6 +6,38 @@ import {
 import { fetchNotes } from '@/lib/api';
 import NotesClient from './Notes.client';
 import { NoteTag } from '@/types/note';
+import { Metadata } from 'next';
+
+
+ export const generateMetadata =async ({ params }:  Props):Promise<Metadata>=>{
+  const{slug}=await params;
+ const category = slug[0] === 'all' ? 'All Notes' : slug[0];
+
+return {
+  title: `Notes: ${category}`,
+ description: `Viewing notes in the ${category} category.`,
+ openGraph:{
+title:`Notes: ${category}`,
+description: `Viewing notes in the ${category} category.`,
+url: `https://notehub.com/notes/filter/${category}`,
+ images:[
+  {
+    url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+    width: 1200,
+    height: 630,
+    alt: `Notes: ${category}`
+  }
+ ]
+
+
+ }
+}
+
+ }
+
+
+
+
 
 type Props = {
   params: Promise<{ slug: [NoteTag | 'all'] }>;
